9a8882efd78af2ef7f6727c29cb30e063dcc12b2
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2020-06-10 00:48:28 -0300

    validando campos...

--- js/app.js
+++ js/app.js
@@ -19,6 +19,11 @@ eventListeners();
 function eventListeners(){
     //inicio de la aplicacion y deshabilitar submit
     document.addEventListener('DOMContentLoaded', inicioApp);
+
+    //Campos de formulario
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
 }
 
 
@@ -30,5 +35,47 @@ function eventListeners(){
 function inicioApp(){
     //deshabilita el envio
     btnenviar.disabled = true;
+
+}
+
+// valida que el campo tenga algo escrito
+function validarCampo(){
+
+    //se valida la longitud del texto y que no este vacio
+    validarLogitud(this);
+
+    //Validar unicamente el email
+    if(this.type === 'email'){
+        validar(this);
+    }
+
+    let errores = document.querySelectorAll('.error');
+    if(email.value  !== '' && asunto.value !== '' && mensaje.value !== ''){
+        if(errores.length === 0){
+            btnenviar.disabled = false;
+        }
+        
+    }
+}
+
+function validarCampo(campo){
+        if(campo.value.length > 0){
+            campo.style.borderBottomColor = green;
+            campo.classList.remove('error');
+        }else{
+            campo.style.borderBottomColor = red;
+            campo.classList.add('error');
+        }
+}
+
+function validarEmail(campo){
+    const mensaje = campo.value;
+    if(mensaje.indexOf('@') !== -1 ){
+        campo.style.borderBottomColor = green;
+            campo.classList.remove('error');
+    }else{
+        campo.style.borderBottomColor = red;
+            campo.classList.add('error');
+    }
 
 }
